f4f3a9e808e78ef2d0b5ea85c85637010a86b840
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-08 15:54:34 +0900

    [...] 스크립트 이름 정정

--- Assets/Scripts/0_JeongMyeongJin_RefCode/LoadSpriteTest.cs
+++ Assets/Scripts/0_JeongMyeongJin_RefCode/LoadSpriteTest.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class LoadSpriteTest : MonoBehaviour
 {
     [SerializeField] private Image Image_LoadSampleTest;
 
