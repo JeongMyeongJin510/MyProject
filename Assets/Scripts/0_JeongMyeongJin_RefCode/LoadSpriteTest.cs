3dded9d664cbe411de5e5dd2820a67c6b2246ddb
JeongMyeongJin510 <audwls3159@gmail.com>
2026-05-11 16:18:37 +0900

    [로딩 팝업] RawImage 사용하여 로딩 화면에 이미지 불러오기, 로딩바 구현을 위해 Coroutine 사용

--- Assets/Scripts/0_JeongMyeongJin_RefCode/LoadSpriteTest.cs
+++ Assets/Scripts/0_JeongMyeongJin_RefCode/LoadSpriteTest.cs
@@ -1,18 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadSpriteTest : MonoBehaviour
 {
-    [SerializeField] private Image Image_LoadSampleTest;
+    [SerializeField] private RawImage RawImage_LoadingImg;
+    [SerializeField] private Slider Slider_LoadingBar;
+
 
     private void OnEnable()
     {
-        Sprite loadedSprite = Resources.Load<Sprite>("2D/Background");
-        if (loadedSprite != null)
-        {
-            Image_LoadSampleTest.sprite = loadedSprite;
-        }
+        LoadAndSetLoadingImg();
+        StartCoroutine(CoStrartLoadingBarEffect());
+    }
 
+    private void LoadAndSetLoadingImg()
+    {
+        var texture = Resources.Load<Texture>("Texture/Texture_LoadingImage");
+        if (texture != null)
+        {
+            RawImage_LoadingImg.texture = texture;
+        }
+    }
+
+    IEnumerator CoStrartLoadingBarEffect()
+    {
+        Slider_LoadingBar.value = 0.2f;
+        yield return new WaitForSeconds(0.3f);
+
+        Slider_LoadingBar.value = 0.4f;
+
+        yield return new WaitForSeconds(0.5f);
+
+        Slider_LoadingBar.value = 0.7f;
+
+        yield return new WaitForSeconds(1.0f);
+
+        Slider_LoadingBar.value = 1.0f;
+
+        yield return new WaitForSeconds(1.3f);
+
+
+        this.gameObject.SetActive(false);
 
     }
 }
